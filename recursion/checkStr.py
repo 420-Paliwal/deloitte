defffade9469fd02225a5b3735243c1f4dbc7e1d
420-Paliwal <harshpaliwal0420@gmail.com>
2025-10-30 18:57:26 +0530

    isPalindrome ? 2nd method

--- recursion/checkStr.py
+++ recursion/checkStr.py
@@ -11,3 +11,11 @@ def pName(arr):
 
 summ = pName("harsh")
 print(summ)
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        newStr = ""
+        for i in s:
+            if i.isalnum():
+                newStr += i.lower()
+        return newStr == newStr[::-1]
