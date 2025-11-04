97f9c64d8fe8e9b4e6abdb1b6df8c8c3dd0bcb8f
420-Paliwal <harshpaliwal0420@gmail.com>
2025-11-04 09:13:12 +0530

    check array

--- array/sorted.py
+++ array/sorted.py
@@ -1,7 +1,9 @@
-class Solution:
-    def check(self, arr: List[int]) -> bool:
-        n = len(arr)
-        for i in range(1,n):
-            if arr[i-1] > arr[i]:
-                return False
-        return True
+def check(arr):
+    n = len(arr)
+    for i in range(1,n):
+        if arr[i-1] > arr[i]:
+            return False
+    return True
+
+checked = check([1, 2, 2, 3, 4])
+print(checked)
