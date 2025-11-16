9bf8bb22deeff282a6f73d6ec4b407d803f39ac7
420-Paliwal <harshpaliwal0420@gmail.com>
2025-11-16 13:02:48 +0530

    remove duplicate

--- array/remove_dup.py
+++ array/remove_dup.py
@@ -1,13 +1,15 @@
 class Solution:
-    def removeDuplicates(self, arr: list[int]) -> int:
-        count = 1 
+    def removeDuplicates(self, arr):
         n = len(arr)
         j = 0
         for i in range(1, n):
             if arr[i] != arr[j]:
-                count += 1
-                j = i
-        return count
+                j += 1
+                arr[j] = arr[i]
+            arr[i] = None
+        return j + 1
     
 obj = Solution()
-print(obj.removeDuplicates([1, 1, 2, 2, 3, 4, 4, 5]))
+arr = [1, 1, 2, 2, 3, 4, 4, 5]
+print(obj.removeDuplicates(arr))
+print(arr)
