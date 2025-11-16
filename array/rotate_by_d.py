65c48e73c81b7ba2e6512e3884d8ceba9e6bafa4
420-Paliwal <harshpaliwal0420@gmail.com>
2025-11-16 13:42:09 +0530

    rotate by d

---
+++ array/rotate_by_d.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rotate(self, arr, d):
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(arr)
+        while (d >= 0):
+            temp = arr[0]
+            for i in range(n-1):
+                arr[i] = arr[i+1]
+            arr[n-1] = temp
+            d -= 1
+        return arr
+    
+obj = Solution()
+print(obj.rotate([1, 2, 3, 4, 5], 2))
