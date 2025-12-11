fe6c305f6e78c69ff3e3e77dc2643e2ccfa2cfd7
420-Paliwal <harshpaliwal0420@gmail.com>
2025-12-11 09:26:46 +0530

    longest consecutive optimized

--- array/longestConsecutive.py
+++ array/longestConsecutive.py
@@ -1,19 +1,33 @@
 class Solution:
     def longestConsecutive(self, arr):
-        arr.sort()
-        n = len(arr)
-        count = 1
-        lgs = 1
-        for i in range(1,n):
-            if arr[i] == arr[i-1] + 1:
-                count += 1
-            elif arr[i] == arr[i-1]:
+        # arr.sort()
+        # n = len(arr)
+        # count = 1
+        # lgs = 1
+        # for i in range(1,n):
+        #     if arr[i] == arr[i-1] + 1:
+        #         count += 1
+        #     elif arr[i] == arr[i-1]:
+        #         continue
+        #     else:
+        #         count = 1
+        #     if count > lgs:
+        #         lgs = count
+        # return lgs
+        arr = set(arr)
+        lg = 1
+        for i in arr:
+            if i-1 in arr:
                 continue
             else:
                 count = 1
-            if count > lgs:
-                lgs = count
-        return lgs
+                temp = i+1
+                while(temp in arr):
+                    count+=1
+                    temp+=1
+                if count > lg:
+                    lg = count
+        return lg 
 
 obj = Solution()
 arr = [100, 4, 200, 1, 3, 2]
